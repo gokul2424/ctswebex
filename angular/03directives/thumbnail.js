e29aad23f8dbff0adcaf79701f3dc5ccf78c8f00
puneet <puneetvashsiht@gmail.com>
2017-07-06 15:16:05 +0530

    spa

--- angular/03directives/thumbnail.js
+++ angular/03directives/thumbnail.js
@@ -1,22 +1,44 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ui.bootstrap']);
 
-        app.directive('cgThumbnail', function(){
-            return {
-                restrict : 'E',
-                templateUrl: 'tags/thumbnail.html',
-                scope: {
-                    title: '@title',
-                    summary: '@summary'
-                }
-            }
-        })
+app.directive('cgThumbnail', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'tags/thumbnail.html',
+        scope: {
+            title: '@title',
+            summary: '@summary'
+        }
+    }
+})
 
-        app.controller('ThumbCtrl', function($scope){
-            $scope.courses = [
-                {title: 'AngularJS', summary:'Its a framework from Google'},
-                {title: 'ReactJS', summary:'Its a library from Facebook'},
-                {title: 'EmberJS', summary:'Its a framework and open-source'},
-                {title: 'CanJS', summary:'Its a framework and really good'},
-                {title: 'ExtJS', summary:'Meant for business apps'}
-            ]
-        })
+app.controller('ThumbCtrl', function ($scope, $http) {
+
+    $http({
+        url: "courses.json",
+        method: 'GET'
+    }).then(function (response) {
+        console.log(response.data);
+        $scope.courses = response.data
+    });
+
+
+    $scope.rate = 7;
+    $scope.max = 10;
+    $scope.isReadonly = false;
+
+    $scope.hoveringOver = function (value) {
+        $scope.overStar = value;
+        $scope.percent = 100 * (value / $scope.max);
+    };
+
+    $scope.ratingStates = [
+        { stateOn: 'glyphicon-ok-sign', stateOff: 'glyphicon-ok-circle' },
+        { stateOn: 'glyphicon-star', stateOff: 'glyphicon-star-empty' },
+        { stateOn: 'glyphicon-heart', stateOff: 'glyphicon-ban-circle' },
+        { stateOn: 'glyphicon-heart' },
+        { stateOff: 'glyphicon-off' }
+    ];
+
+
+    // $scope.courses = 
+})
